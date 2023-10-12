88263c70550411ca7a58d4a4cf852725b698a973
Rudolf Bruder <rudolf.bruder@slovar.sk>
2023-10-12 15:22:53 +0200

    Test of token

--- src/helpers.php
+++ src/helpers.php
@@ -5,6 +5,8 @@ use Ravols\ElasticInsight\Modules\Elastic\ElasticSearchModule;
 if (! function_exists('elasticSearchModuleRavols')) {
     function elasticSearchModuleRavols(): ElasticSearchModule
     {
+        dd(1);
+
         return app()['elasticInsightElasticSearchModule'];
     }
 }
