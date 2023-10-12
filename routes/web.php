e012d64535c7bbe3572f79f815daeadeceff1f9e
rudolfbruder <rudolfbruder@users.noreply.github.com>
2023-10-12 11:04:34 +0000

    Fix styling

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,14 @@ use Ravols\ElasticInsight\Http\Controllers\DashboardController;
 use Ravols\ElasticInsight\Http\Controllers\Indices\AllAliasesController;
 use Ravols\ElasticInsight\Http\Controllers\Indices\AllIndicesController;
 
-Route::get('elastic-insight', [DashboardController::class,'index']);
+Route::get('elastic-insight', [DashboardController::class, 'index']);
 Route::group(['prefix' => 'elastic-insight', 'as' => 'elastic-insight.'], function (): void {
     Route::group(['prefix' => 'indices', 'as' => 'indices.'], function (): void {
-        Route::get('all-indices', [AllIndicesController::class,'index'])->name('index');
+        Route::get('all-indices', [AllIndicesController::class, 'index'])->name('index');
     });
     Route::group(['prefix' => 'aliases', 'as' => 'aliases.'], function (): void {
-        Route::get('all-aliases', [AllAliasesController::class,'index'])->name('index');
+        Route::get('all-aliases', [AllAliasesController::class, 'index'])->name('index');
     });
-    Route::get('elastic-insight', [DashboardController::class,'index']);
+    Route::get('elastic-insight', [DashboardController::class, 'index']);
 
 });
