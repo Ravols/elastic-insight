e9b3e9c611509ba96b963843c3f40f2b2571ebb2
Rudolf Bruder <rudolf.bruder@slovar.sk>
2023-10-12 15:59:31 +0200

    Clean up

--- src/helpers.php
+++ src/helpers.php
@@ -5,8 +5,6 @@ use Ravols\ElasticInsight\Modules\Elastic\ElasticSearchModule;
 if (! function_exists('elasticSearchModuleRavols')) {
     function elasticSearchModuleRavols(): ElasticSearchModule
     {
-        dd(1);
-
         return app()['elasticInsightElasticSearchModule'];
     }
 }
