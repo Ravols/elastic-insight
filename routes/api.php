72be25e21679a462885ee6dd5af17779e0ebe287
Jaroslav Stefanec <55752260+jaroslavstefanec@users.noreply.github.com>
2024-04-03 14:59:12 +0200

    u

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Ravols\ElasticInsight\Http\Controllers\Api\AllIndicesController; 
+use Ravols\ElasticInsight\Http\Controllers\Api\AllIndicesController;
 
 Route::group(['middleware' => 'api', 'prefix' => 'elastic-insight/api'], function (): void {
     Route::get('indices', [AllIndicesController::class, 'index']);
